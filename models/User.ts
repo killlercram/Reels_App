375a4a557c18c7b6000f85f7a32fc007574d2ac3
killlercam <herosviral@gmail.com>
2025-02-21 23:25:07 +0530

    Learned how to make models in next.js keeping the typescript in mind and also learned how to do hashing for the password with a library bcryptjs and also learned about the middleware hooks in of nextjs that is pre

---
+++ models/User.ts
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import mongoose, { Schema ,model,models} from "mongoose";
+import bcrypt from "bcryptjs";
+
+export interface IUser{
+  email: string;
+  password: string;
+  _id?: mongoose.Types.ObjectId;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const userSchema=new Schema<IUser>(
+  {
+    email:{type: String, required: true, unique: true},
+    password: {type: String,required: true}
+  },
+  {timestamps: true}//this will update createdAt and UpdatedAt 
+);
+//for encrypting passwords when it is modified
+userSchema.pre("save",async function (next) {
+  if(this.isModified("password")){
+    this.password=await bcrypt.hash(this.password,10);
+  }
+  next();
+});
+
+//Exporting the model
+const User =models?.User || model<IUser>("User",userSchema);
+export default User;
